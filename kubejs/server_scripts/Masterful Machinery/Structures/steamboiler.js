553c47d4f2619f88b6af942b903e2a807bff83b8
MrTK-Dev <Dev.MrTK@gmail.com>
2022-02-04 18:04:22 +0100

    test for new mm machine

--- kubejs/server_scripts/Masterful Machinery/Structures/steamboiler.js
+++ kubejs/server_scripts/Masterful Machinery/Structures/steamboiler.js
@@ -1,33 +1,33 @@
-onEvent('recipes', e => { 
-  e.custom({
-    type: 'masterfulmachinery:machine_structure',
-    controllerId: 'basic',
-    name: 'Steam Boiler',
-    id: 'steam_boiler',
-  layout: [
-    ['AAA', 'AAA', 'AAA'],
-    ['AEA', 'FBO', 'ACA'],
-    ['AAA', 'ASA', 'AAA']
-  ],
-  'legend': {
-    'A': {
-      'block': 'kubejs:modularium_casing'
-    },
-    'S': {
-      'block': 'masterfulmachinery:basic_basic_port_energy_input'
-    },
-    'B': {
-      'block': 'kubejs:modularium_heating_core'
-    },
-    'F': {
-      'block': 'masterfulmachinery:basic_basic_port_fluids_input'
-    },
-    'E': {
-      'block': 'masterfulmachinery:basic_basic_port_items_input'
-    },
-    'O': {
-      'block': 'masterfulmachinery:basic_basic_port_fluids_output'
-    }
-  }
-  })
-})
+// onEvent('recipes', e => { 
+//   e.custom({
+//     type: 'masterfulmachinery:machine_structure',
+//     controllerId: 'basic',
+//     name: 'Steam Boiler',
+//     id: 'steam_boiler',
+//   layout: [
+//     ['AAA', 'AAA', 'AAA'],
+//     ['AEA', 'FBO', 'ACA'],
+//     ['AAA', 'ASA', 'AAA']
+//   ],
+//   'legend': {
+//     'A': {
+//       'block': 'kubejs:modularium_casing'
+//     },
+//     'S': {
+//       'block': 'masterfulmachinery:basic_basic_port_energy_input'
+//     },
+//     'B': {
+//       'block': 'kubejs:modularium_heating_core'
+//     },
+//     'F': {
+//       'block': 'masterfulmachinery:basic_basic_port_fluids_input'
+//     },
+//     'E': {
+//       'block': 'masterfulmachinery:basic_basic_port_items_input'
+//     },
+//     'O': {
+//       'block': 'masterfulmachinery:basic_basic_port_fluids_output'
+//     }
+//   }
+//   })
+// })
