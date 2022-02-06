ce8af87cd669195dfaf5d2673a12694b6c25c1f2
MrTK-Dev <Dev.MrTK@gmail.com>
2022-02-06 18:29:21 +0100

    First version of steam boiler is working

--- kubejs/server_scripts/Crafting Recipes/masterfulmachinery.js
+++ kubejs/server_scripts/Crafting Recipes/masterfulmachinery.js
@@ -7,7 +7,7 @@ settings.logErroringRecipes = true
 
 onEvent('recipes', e => {
 		//Basic Controller
-	e.shaped('masterfulmachinery:basic_controller', ['ABA', 'BDB', 'ACA'], {
+	e.shaped('masterfulmachinery:steam_boiler_controller', ['ABA', 'BDB', 'ACA'], {
 		A: 'kubejs:modularium_casing',
 		B: '#forge:plates/silver',
 		C: 'immersiveengineering:rs_engineering',
--- kubejs/server_scripts/Masterful Machinery/Recipes/steamboiler.js
+++ kubejs/server_scripts/Masterful Machinery/Recipes/steamboiler.js
@@ -1,45 +1,43 @@
 onEvent('recipes', e => {
-  let boiler_recipe = (fuel, fuelamount, input, time, amount) => {
+  let steam_generation = (input_coke, input_cokecount, input_fuel, input_fuelamount, time, output, outputamount, recipe_id) => {
     e.custom({
       type: 'masterfulmachinery:machine_process',
       structureId: 'steam_boiler',
-      controllerId: 'basic',
-      ticks: time,
-      inputs: [
-        {
-          type: 'masterfulmachinery:items',
-          data: {
-              tag: fuel,
-              count: fuelamount
+      controllerId: 'steam_boiler',
+      outputs: [
+          {
+              type: 'masterfulmachinery:fluids',
+              perTick: true,
+              data: { fluid: output, amount: outputamount }
           }
-        },
-        {
-          type: 'masterfulmachinery:fluids',
-          data: {
-              name: input,
-              amount: 250
-          }
-        },
-        {
-          type: 'masterfulmachinery:energy',
-          data: {
-              amount: 1000
-          }
-      }
       ],
-      outputs:[
-        {
-          type: 'masterfulmachinery:fluids',
-          data: {
-              name: 'mekanism:steam',
-              amount: amount
+      inputs: [
+          {
+              type: 'masterfulmachinery:energy',
+              perTick: true,
+              data: { amount: 1000 }
+          },
+          {
+              type: 'masterfulmachinery:items',
+              perTick: true,
+              data: { tag: input_coke, count: input_cokecount }
+          },
+          {
+              type: 'masterfulmachinery:fluids',
+              perTick: true,
+              data: { fluid: 'minecraft:water', amount: outputamount }
+          },
+          {
+              type: 'masterfulmachinery:fluids',
+              perTick: true,
+              data: { fluid: input_fuel, amount: input_fuelamount }
           }
-        }
-      ]
+      ],
+      ticks: time,
+      id: recipe_id
     })
   }
 
-  //Test
-  boiler_recipe('#forge:charcoal', 8, 'immersiveengineering:creosote', 100, 1000)
-
+  steam_generation('forge:coal_coke', 16, 'immersiveengineering:creosote', 250, 400, 'mekanism:steam', 1000, 'coal_coke')
+  steam_generation('forge:coal_petcoke', 8, 'immersiveengineering:creosote', 250, 400, 'mekanism:steam', 1000, 'coal_coke')
 })
