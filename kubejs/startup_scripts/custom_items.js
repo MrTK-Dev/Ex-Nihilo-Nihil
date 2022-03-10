9e8e1106c243a8fd4d0bd5f49200fb40b4b7ab7c
MrTK-Dev <Dev.MrTK@gmail.com>
2022-03-10 21:04:42 +0100

    fix custom items

--- kubejs/startup_scripts/custom_items.js
+++ kubejs/startup_scripts/custom_items.js
@@ -2,35 +2,23 @@
 
 onEvent('item.registry', e => { 
   const basic_items = [
-    {
-      registry: 'modularium_ingot',
-      display: 'Modularium Ingot'
-    },
-    {
-      registry: 'screen',
-      display: 'Screen'
-    },
-    {
-      registry: 'heatproof_brick',
-      display: 'Heatproof Brick'
-    },
-    {
-      registry: 'string_fluxed',
-      display: 'Fluxed String'
-    }
+    'modularium_ingot',
+    'screen',
+    'heatproof_brick',
+    'string_fluxed',
   ]
 
   basic_items.forEach((item) => {
-    e.create(item.registry).group('KubeJS')
+    e.create(item).group('KubeJS')
   })
 
   const infused_crystals = [
-    'Blazing',
-    'Niotic',
-    'Spirited'
+    'blazing',
+    'niotic',
+    'spirited'
   ]
 
   infused_crystals.forEach((crystal) => {
-    e.create(`crystal_${crystal.toLowerCase()}_infused`).displayName(`Infused ${crystal} Crystal`).glow(true).rarity('uncommon')
+    e.create(`crystal_${crystal}_infused`).glow(true).rarity('rare')
   })
 })
